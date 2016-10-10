b72f9a9e2df0dd33bcae571c08cf55e430f5d982
arthurlee <boyal2008@gmail.com>
2016-10-10 17:26:43 +0800

    new route mechanism

--- samples/hello/resources/hello.go
+++ samples/hello/resources/hello.go
@@ -1,16 +1,14 @@
 package resources
 
 import (
-	"encoding/json"
-	"net/http"
+	"github.com/arthurlee/goa/route"
 )
 
-func HandleHello(w http.ResponseWriter, r *http.Request) {
+func HandleHello(res *route.GoaResponse) {
 	person := struct {
 		Name string `json:"name", db:"pk"`
 		Age  int    `json:"age"`
 	}{"Tom", 12}
 
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(person)
+	res.SendJson(person)
 }
--- samples/hello/resources/route_register.go
+++ samples/hello/resources/route_register.go
@@ -1,11 +1,11 @@
 package resources
 
 import (
-	"github.com/arthurlee/goa-doc/samples/hello/resources/course"
+	//"github.com/arthurlee/goa-doc/samples/hello/resources/course"
 	"github.com/arthurlee/goa/route"
 )
 
 func Register() {
-	route.Register("/", HandleHello)
-	route.Register("/course/list", course.List)
+	route.Get("/", HandleHello)
+	//route.Get("/course/list", course.List)
 }
