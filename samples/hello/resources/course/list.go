3b75dff54451d951a00d26ca6e8fbdc83dfaae3f
arthurlee <boyal2008@gmail.com>
2016-10-10 17:50:12 +0800

    new route mechanism: database ok

--- samples/hello/resources/course/list.go
+++ samples/hello/resources/course/list.go
@@ -1,45 +1,24 @@
 package course
 
 import (
-	"encoding/json"
 	"github.com/arthurlee/goa-doc/samples/hello/models"
 	"github.com/arthurlee/goa/database"
+	"github.com/arthurlee/goa/route"
 	//"log"
-	"net/http"
 )
 
-type GoaBaseRes struct {
-	Code    string `json:"code"`
-	Message string `json:"message"`
-}
-
-func (res *GoaBaseRes) setError(code string, message string) {
-	res.Code = code
-	res.Message = message
-}
-
-func (res *GoaBaseRes) setOK() {
-	res.Code = "0"
-	res.Message = "OK"
-}
-
 type CourseListRes struct {
-	GoaBaseRes
-	Courses []models.Course `json:"course_list"`
+	route.GoaBaseRes
+	Courses []models.Course `json:"courses"`
 }
 
-func List(w http.ResponseWriter, r *http.Request) {
-	resCourseList := CourseListRes{}
-
+func List(res *route.GoaResponse) {
 	courseList := models.NewCourseList()
 	err := database.GetList(courseList)
 	if err != nil {
-		resCourseList.setError("1", err.Error())
+		res.SendError("1", err.Error())
 	} else {
-		resCourseList.Courses = courseList.Courses
-		resCourseList.setOK()
+		resCourseList := CourseListRes{route.GoaBaseRes{"0", "ok"}, courseList.Courses}
+		res.SendJson(resCourseList)
 	}
-
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(resCourseList)
 }
--- samples/hello/resources/route_register.go
+++ samples/hello/resources/route_register.go
@@ -1,11 +1,11 @@
 package resources
 
 import (
-	//"github.com/arthurlee/goa-doc/samples/hello/resources/course"
+	"github.com/arthurlee/goa-doc/samples/hello/resources/course"
 	"github.com/arthurlee/goa/route"
 )
 
 func Register() {
 	route.Get("/", HandleHello)
-	//route.Get("/course/list", course.List)
+	route.Get("/course/list", course.List)
 }
