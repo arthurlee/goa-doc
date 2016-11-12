a293de4b519336a27b2ede0b865b43f97bdb1e09
arthurlee <boyal2008@gmail.com>
2016-11-12 15:12:06 +0800

    new register methods

--- samples/hello/resources/route_register.go
+++ samples/hello/resources/route_register.go
@@ -3,10 +3,23 @@ package resources
 import (
 	"github.com/arthurlee/goa-doc/samples/hello/resources/course"
 	"github.com/arthurlee/goa/route"
+	"github.com/arthurlee/goa/server"
 )
 
 func Register() {
 	route.Get("/", HandleHello)
+
+	// test script:  http http://127.0.0.1:5400/heartbeat
+	route.Get("/heartbeat", func(res *server.GoaResponse) {
+		res.SendOK()
+	})
+
+	// test script:  http http://127.0.0.1:5400/welcome
+	route.Get("/welcome", server.GetSendOK())
+
+	// test script:  http http://127.0.0.1:5400/error
+	route.Get("/error", server.GetSendError("-1", "You are wrong!"))
+
 	route.Get("/course/list", course.List)
 	route.Post("/course/create", course.Create)
 	route.Post("/course/update", course.Update)
